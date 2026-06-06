eaec87d1dfda09eedbfa06735669caec18dffb2e
ZuoCen Liu <liouzuocen@qq.com>
2026-06-06 12:33:21 +0800

    Add console script entry for ai_latency_injector_node

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     tests_require=["pytest"],
     entry_points={
         "console_scripts": [
+            "ai_latency_injector_node = ai_flight_integrity_observer.ai_latency_injector_node:main",
             "flight_integrity_node = ai_flight_integrity_observer.flight_integrity_node:main",
             "synthetic_px4_publisher = ai_flight_integrity_observer.synthetic_px4_publisher:main",
             "flight_diagnostics_to_csv_labeler = ai_flight_integrity_observer.flight_diagnostics_to_csv_labeler:main",
